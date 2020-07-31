784b2f2fe6caf0e2474f17a5ae4a334bbd8c6a73
magdakok <61601293+magdakok@users.noreply.github.com>
2020-07-31 22:37:49 +0100

    refactor animation script to make it reusable

--- js/animation.js
+++ js/animation.js
@@ -1,12 +1,14 @@
 const lis = Array.from(document.querySelectorAll(".messages-box>li"));
-const initDelay = 1000;
-const delay = 400;
 
-lis.forEach((li, i) => {
-  setTimeout(
-    () => {
-      li.classList.add("sequence-animation");
-    },
-    i === 0 ? initDelay : initDelay + delay * i
-  );
-});
+const sequenceAnimation = (elements, initDelay, delay) => {
+  elements.forEach((el, i) => {
+    setTimeout(
+      () => {
+        el.classList.add("sequence-animation");
+      },
+      i === 0 ? initDelay : initDelay + delay * i
+    );
+  });
+};
+
+sequenceAnimation(lis, 1000, 400);
