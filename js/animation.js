31eafbeb17bd6f4272339bf1efecc0377cf3f33b
magdakok <61601293+magdakok@users.noreply.github.com>
2020-07-31 22:26:28 +0100

    add initial delay for animation

--- js/animation.js
+++ js/animation.js
@@ -1,8 +1,12 @@
 const lis = Array.from(document.querySelectorAll(".messages-box>li"));
+const initDelay = 1000;
 const delay = 400;
 
 lis.forEach((li, i) => {
-  setTimeout(() => {
-    li.classList.add("sequence-animation");
-  }, delay * i);
+  setTimeout(
+    () => {
+      li.classList.add("sequence-animation");
+    },
+    i === 0 ? initDelay : initDelay + delay * i
+  );
 });
